fdcf7c631107cbedef3d2a8681cbaa524f14dad1
Sh4dowGaming <49210023+Sh4dowGaming@users.noreply.github.com>
2019-04-26 16:34:57 +1000

    Create caravan.js

---
+++ caravan.js
@@ -0,0 +1,26 @@
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+var OregonH = OregonH || {};
+ 
+OregonH.Caravan = {};
+ 
+OregonH.Caravan.init = function(stats){
+  this.day = stats.day;
+  this.distance = stats.distance;
+  this.crew = stats.crew;
+  this.food = stats.food;
+  this.oxen = stats.oxen;
+  this.money = stats.money;
+  this.firepower = stats.firepower;
+};
